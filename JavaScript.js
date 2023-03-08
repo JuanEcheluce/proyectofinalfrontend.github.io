96169d789c2bb8b8b06afdb0a8e306749707d9cd
Juancho1982 <108824439+Juancho1982@users.noreply.github.com>
2023-03-08 17:17:53 -0300

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,6 +1,5 @@
 document.getElementById('boton_mostrar').addEventListener('click', function(){
     console.log("Evento01");
-    document.getElementById('datosdecontacto').style.display = "block";
-    document.getElementById('subtitulo_contacto').innerHTML = "Datos de Contacto";
+    document.getElementById('listacontacto').style.display = "block";
 });
 
